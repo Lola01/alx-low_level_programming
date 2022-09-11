3af72df481c698460a8d642ff213025ba446f7c0
Ajuwon Oluwadamilola <damilolaajuwon99@gmail.com>
2022-09-11 13:58:03 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle")
+#include <stdio.h>
+
+/**
+ * main - print the string
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
