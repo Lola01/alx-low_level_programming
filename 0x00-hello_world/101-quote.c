53e112a064d53903d2d9299d44b8152fdb1a4d7d
Ajuwon Oluwadamilola <damilolaajuwon99@gmail.com>
2022-09-12 19:29:06 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - Prints the string
  *
- * Return: Always 1 (Successful) 
+ * Return: Always 1 (Successful)
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
