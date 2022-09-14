585e6b634c079a04cc443d428454c48a97f9bdb6
Ajuwon Oluwadamilola <damilolaajuwon99@gmail.com>
2022-09-13 20:05:05 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always (successful)
+ */
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+
+	return (0);
