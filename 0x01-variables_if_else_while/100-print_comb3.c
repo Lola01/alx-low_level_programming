a58b63547be339c4bf63aeb152429066a4c31576
Ajuwon Oluwadamilola <damilolaajuwon99@gmail.com>
2022-09-14 02:16:49 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = '1'; y <= '9'; y++)
+		{
+			if (y > x)
+			{
+				putchar(x + '0');
+
+				putchar(y + '0');
+
+				if (x != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
