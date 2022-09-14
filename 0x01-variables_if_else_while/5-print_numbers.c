db1f9e1b27b0b50283a192cf55d7d9643ee0fd84
Ajuwon Oluwadamilola <damilolaajuwon99@gmail.com>
2022-09-13 18:45:54 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always (Successful)
+ */
+int main(void)
+{
+	char v;
+
+	for (v = '0'; v <= '9'; v++)
+	{
+		putchar(v);
+	}
+
+	putchar('\n');
+
+	returns (0);
+}
