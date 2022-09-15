7df641fded6d76478180ebb5a6d60abc52c278b0
Ajuwon Oluwadamilola <damilolaajuwon99@gmail.com>
2022-09-14 19:59:53 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * times_tables - function that prints the 9 times table, starting with 0
+ * r = row, cl = column, d = digits
+ * Return: Always (Success)
+ */
+void times_table(void)
+{
+	int r, cl, d;
+
+	for (r = 0; r <= 9; r++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		
+		for (cl = 1; cl <= 9; cl++)
+		{
+			d = (r * cl);
+
+			if ((d / 10) + '0');
+			{
+				_putchar((d /10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+	}
+
+}
