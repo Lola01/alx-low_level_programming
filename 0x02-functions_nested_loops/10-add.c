cf51e270f1f689d922c7f6134b4d9d7d93d5f62f
Ajuwon Oluwadamilola <damilolaajuwon99@gmail.com>
2022-09-14 21:15:53 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - a function that adds two integers and returns the result
+ * @na: the first integer
+ * @nb: the secong integer
+ * Return: Always (Success)
+ */
+int add(int na, int nb)
+{
+	return (na + nb);
+}
