3333cae4f85235ec7b5d7734d8dfb3bad1737183
Ajuwon Oluwadamilola <damilolaajuwon99@gmail.com>
2022-09-14 22:01:38 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,21 +11,21 @@ void times_table(void)
 	for (r = 0; r <= 9; r++)
 	{
 		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
 
 		for (cl = 1; cl <= 9; cl++)
 		{
-			d = (r * cl);
+			_putchar(',');
+			_putchar(' ');
 
-			if ((d / 10) < 0)
-			{
-				_putchar((d / 10) + '0');
-			}
-			else
-			{
+			d = r * cl;
+
+			if (d <= 9)
+			
 				_putchar(' ');
-			}
+			else
+				_putchar((d / 10) + '0');
+			
+			_putchar((d % 10) + '0');
 		}
 		_putchar('\n');
 	}
