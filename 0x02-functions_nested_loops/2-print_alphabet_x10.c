ea51dfd60b13d7b7502c75088ae720d88aeb0236
Ajuwon Oluwadamilola <damilolaajuwon99@gmail.com>
2022-09-14 18:01:17 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -  prints 10 times the alphabet
+ *
+ * Return: Always (Sucess)
+ */
+void print_alphabet_x10(void)
+{
+	int c, d;
+	d = 0;
+
+	while (d < 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		
+		d++;
+
+		_putchar('\n');
+	}
+}
