0a3dd5d39c1e3b1ddef5122ed93bd9d9dafced7b
Ajuwon Oluwadamilola <damilolaajuwon99@gmail.com>
2022-09-20 20:51:27 -0700

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,18 @@
 #include "main.h"
 
 /**
+ * _puts - function that prints a string, followed by a new line, to stdout
+ * @str: The string to print
  *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int index;
+
+	for (index = 0; str[index] != '\0'; index++)
+	{
+		_putchar(str[index]);
+	}
+	_putchar('\n');
+}
