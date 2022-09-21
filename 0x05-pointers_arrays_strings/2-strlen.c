569bb94985d8fd2b1a13df1c252a81e72c3d9058
Ajuwon Oluwadamilola <damilolaajuwon99@gmail.com>
2022-09-20 20:38:51 -0700

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,11 +2,11 @@
 
 /**
  * int _strlen - function that returns the length of a string
- * @s - this is the input string
+ * @s: this is the input string
  *
  * Return: Length of string
  */
-int _strlen(char *s);
+int _strlen(char *s)
 {
 	int index;
 
