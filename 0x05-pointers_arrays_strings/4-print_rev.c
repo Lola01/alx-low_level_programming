d91e1636cc92283e51fe8abe0b1a73581b2d26dc
Ajuwon Oluwadamilola <damilolaajuwon99@gmail.com>
2022-09-20 21:03:34 -0700

    Iask 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * print_rev - function that prints a string, in reverse, followed by a new line
+ * print_rev - print a string, in reverse, followed by a new line
  * @s: This is the input string
- * 
  * Return: void
  */
 void print_rev(char *s)
