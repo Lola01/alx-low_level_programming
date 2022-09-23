e19e683ee54e4291a284d80bd9b1e76d964c29a5
Ajuwon Oluwadamilola <damilolaajuwon99@gmail.com>
2022-09-23 13:12:24 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,7 +5,7 @@
  * _strcat - concatenates two strings
  * @dest: the destination string
  * @src: the source string
- * Return: A pointer to the resulting string dest
+ * Return: A pointer to the resulting string @dest
  */
 char *_strcat(char *dest, char *src)
 {
