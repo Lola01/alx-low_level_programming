3a3314d42aeb49fb4508c8b1152ec0fbe08b0899
Ajuwon Oluwadamilola <damilolaajuwon99@gmail.com>
2022-09-23 20:58:06 -0700

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,7 +4,6 @@
  * reverse_array - a function that reverses the content of an array of integers
  * @a: array of integers
  * @n: number of elements
- * Return: empty
  */
 void reverse_array(int *a, int n)
 {
