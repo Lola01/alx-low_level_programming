0bb6e88818262febf42573e0bb8270f12e289be8
Ajuwon Oluwadamilola <damilolaajuwon99@gmail.com>
2022-09-28 20:36:32 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/**_puts_recursion - a function that prints a string, followed by a new line
+/**
+ * _puts_recursion - a function that prints a string, followed by a new line
  * @s: memory to fill
  * Return: void
  */
