d8621fae42deb660e09f1d589eb0891fd763c54b
Ajuwon Oluwadamilola <damilolaajuwon99@gmail.com>
2022-10-12 21:51:30 -0700

    task 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * get_op_func - returns a pointer to the function of the operator
+ * @s: the operator passed as argument to the program
+ * Return: 0
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int t = 0;
+
+	while (t < 5)
+	{
+		if (strcmp(s, ops[t].op == 0)
+				return (ops[t].f);
+		t++;
+	}
+	return (0);
+}
