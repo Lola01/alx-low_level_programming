aa5bab4c37702993d0ca0510abd184240e185b12
Ajuwon Oluwadamilola <damilolaajuwon99@gmail.com>
2022-10-13 21:02:11 -0700

    Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,6 +4,7 @@
 /**
  * sum_them_all - returns the sum of all its parameters
  * @n: number of arguments
+ * @...: a variable number of parameters
  * Return: sum or 0
  */
 int sum_them_all(const unsigned int n, ...)
