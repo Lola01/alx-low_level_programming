f9ac9736e3b2256962e71c92d264d9049166ccb6
Ajuwon Oluwadamilola <damilolaajuwon99@gmail.com>
2022-10-14 18:48:05 -0700

    Task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,102 +2,48 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-void print_char(va_list arg);
-void print_int(va_list arg);
-void print_float(va_list arg);
-void print_string(va_list arg);
-void print_all(const char * const format, ...);
-
 /**
- * print_char - Prints a char
- * @arg: A list of arguments
- * Return: 0
- */
-void print_char(va_list arg)
-{
-	char w;
-
-	w = va_arg(arg, int);
-	printf("%c", w);
-}
-
-/**
- * print_int - Prints an integer
- * @arg: A list of arguments
- * Return: 0
- */
-void print_int(va_list arg)
-{
-	int x;
-
-	x = va_arg(arg, int);
-	printf("%d", x);
-}
-
-/**
- * print_float - Prints a float
- * @arg: A list of arguments
- * Return: 0
- */
-void print_float(va_list arg)
-{
-	float x;
-
-	x = va_arg(arg, double);
-	printf("%f", x);
-}
-
-/**
- * print_string - Prints a string.
- * @arg: A list of arguments
- * Return: 0
- */
-void print_string(va_list arg)
-{
-	char *y;
-
-	y = va_arg(arg, char *);
-	if (y == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", y);
-}
-
-/**
- * print_all - Prints anything, followed by a new line
- * @format: A string of characters representing the argument types
- * @...: A variable number of arguments to be printed.
- * Return: 0
+ * print_all - a function that prints anything
+ * @format - list of arguments
+ * Return: void
  */
 void print_all(const char * const format, ...)
 {
+	unsigned int m;
 	va_list args;
-	int m = 0, n = 0;
-	char *separator = "";
-	printer_t funcs[] = {
-		{"c", print_char},
-		{"i", print_int},
-		{"f", print_float},
-		{"s", print_string}
-	};
+	char *s, *separator;
 
 	va_start(args, format);
 
-	while (format && (*(format + m)))
-	{
-		n = 0;
+	separator = "";
 
-		while (n < 4 && (*(format + m) != *(funcs[n].symbol)))
-			n++;
-		if (n < 4)
+	m = 0;
+	while (format && format[m])
+	{
+		switch (format[m])
 		{
-			printf("%s", separator);
-			funcs[n].print(args);
-			separator = ", ";
+			case 'c':
+				printf("%s%c", separator, va_arg(args, int);
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(args, int);
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(args, double);
+				break;
+			case 's':
+				s = va_arg(args, char *);
+				if (s == NULL)
+					s = "(nil)";
+				printf("%s%s", separator, s);
+				break;
+			default:
+				m++;
+				continue;
 		}
+		separator = ", ";
 		m++;
+
 	}
 	printf("\n");
 	va_end(args);
