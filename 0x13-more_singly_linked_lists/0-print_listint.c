290492f08288930256baa20c010ad1fe6e8ac82b
Ajuwon Oluwadamilola <damilolaajuwon99@gmail.com>
2022-10-25 20:14:53 -0700

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,12 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
-
+	
+	while (h != NULL)
 	{
-		while (h != NULL)
-		{
-			printf("%i\n", h->n);
-			h = h->next;
-			nodes++;
-		}
+		printf("%i\n", h->n);
+		h = h->next;
+		nodes++;
 	}
 	return (nodes);
 }
