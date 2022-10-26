387e6a085190d467bf28012fda8f644880d16a4e
Ajuwon Oluwadamilola <damilolaajuwon99@gmail.com>
2022-10-25 19:48:05 -0700

    Task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,16 +14,16 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (head == NULL || *head == NULL)
 		return (-1);
+	
 	nxt = *head;
 	prev = NULL;
-	
 	if (index == 0)
 	{
 		*head = (*head)->next;
 		free(nxt);
 		return (1);
 	}
-	for (i = 0; i < index; i +=)
+	for (i = 0; i < index; i += 1)
 	{
 		if (nxt == NULL)
 			return (-1);
