ad53ab22e9286a6f0f852d3660686f573d49ab80
Ajuwon Oluwadamilola <damilolaajuwon99@gmail.com>
2022-10-27 20:00:04 -0700

    Task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * flip_bits - function that returns the number of bits to flip
+ * @n: the number
+ * @m: the number to flip n to
+ * Return: bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int b = n ^ m, bits = 0;
+
+	while (b < 0)
+	{
+		bits += (b & 1);
+		b >>= 1;
+	}
+	return (bits);
+}
